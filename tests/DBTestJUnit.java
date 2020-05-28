420aaf6ead49b52fdbad74930e9448e5539753b6
Faolan7 <matthew@themontneys.com>
2020-05-28 13:17:04 -0700

    Clean up unit tests

--- tests/DBTestJUnit.java
+++ tests/DBTestJUnit.java
@@ -7,9 +7,6 @@ import java.io.PrintStream;
 
 import org.junit.Test;
 
-import maze.MultipleChoice;
-import maze.ShortAnswer;
-import maze.TrueFalse;
 import maze.Main;
 
 
@@ -18,14 +15,11 @@ public class DBTestJUnit {
 	//The Test Question Entrys will add those questions to the DB so be mindful when running
 	//this test. It will add many replicated questions into the question pool.
 	@Test
-	
 	public void test_DBConnection() throws Exception {
-		maze.Main main = new Main();
-		
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         
-        main.connectDatabase();
+        Main.connectDatabase();
         outContent.close();
 		assertEquals("SQLite DB connected\n", outContent.toString());
 		
@@ -34,69 +28,61 @@ public class DBTestJUnit {
 	@Test
 	public void test_TFEntry() throws Exception 
 	{
-		maze.Main main = new Main();
-		main.connectDatabase();
-		main.getDBQuestions();
+		Main.connectDatabase();
+		Main.getDBQuestions();
 		
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         
-		main.insertTFQuestion("Is the sky blue?", "true");
+		Main.insertTFQuestion("Is the sky blue?", "true");
 		
+		assertTrue(outContent.toString().contains("True/False question inserted!"));
 		outContent.close();
-		assertEquals("TF has been inserted!\n", outContent.toString());
 	}
 
 	@Test
 	public void test_MCEntry() throws Exception 
   {
-		maze.Main main = new Main();
-		main.connectDatabase();
-		main.getDBQuestions();
+		Main.connectDatabase();
+		Main.getDBQuestions();
 		String[] incorrectQs = {"red", "green", "yellow"};
 		
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        
-		main.insertMCQuestion("What color is the sky","blue", incorrectQs);
+		System.setOut(new PrintStream(outContent));
+
+		Main.insertMCQuestion("What color is the sky","blue", incorrectQs);
+
+		assertTrue(outContent.toString().contains("Multiple Choice question inserted!"));
 		outContent.close();
-		assertEquals("MC has been inserted!\n", outContent.toString());
 	}
-	
-	
-	
-	
 	
 	@Test
 	public void test_SAEntry() throws Exception 
-  {
-		maze.Main main = new Main();
-		main.connectDatabase();
-		main.getDBQuestions();
+	{
+		Main.connectDatabase();
+		Main.getDBQuestions();
 
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 		
 		Main.insertSAQuestion("What color is blue","blue");
+
+		assertTrue(outContent.toString().contains("Short Answer question inserted!"));
 		outContent.close();
-		assertEquals("SA has been inserted!\n", outContent.toString());
 	}
-	
 	
 	@Test
 	public void test_getDBQuestions() throws Exception 
     {
-		maze.Main main = new Main();
-		main.connectDatabase();
+		Main.connectDatabase();
 		
 		Exception ex = null;
 		
 		try {
-			main.getDBQuestions();
+			Main.getDBQuestions();
 		}catch (Exception e) {
 			ex = e;
 		}
 		assertEquals(null, ex);
 	}
-	
 }
